a2f67fc18b17c144f44650f24837e6cd05294f8e
Damian <damian_l@hotmail.es>
2024-09-28 20:51:59 -0300

    styled-works-cards

--- src/pages/Works.jsx
+++ src/pages/Works.jsx
@@ -30,46 +30,154 @@ function Works() {
           </div>
         </div>
       </div>
-      <div className="grid lg:grid-cols-4 grid-cols-1">
-        <a href="https://www.youtube.com/watch?v=xm9JnzLyNkE">
-          <img src="/images/horas-y-horas.jpg" alt="img" />
-        </a>
-        <a href="https://www.youtube.com/watch?v=HuspyfNwJvE">
-          <img src="/images/talento-y-fe.jpg" alt="img" />
-        </a>
-        <a href="https://www.youtube.com/watch?v=a5Lj3Lnm7LI">
-          <img src="/images/palestina.jpg" alt="img" />
-        </a>
-        <a href="#">
-          <img src="/images/video (12).jpg" alt="img" />
-        </a>
-        <a href="https://www.youtube.com/watch?v=VvUOLPBSY94">
-          <img src="/images/opium.jpg" alt="img" />
-        </a>
-        <a href="https://www.youtube.com/watch?v=_DoXKmH_n5w">
-          <img src="/images/angelina.jpg" alt="img" />
-        </a>
-        <a href="https://www.youtube.com/watch?v=PPbQst-km9c">
-          <img src="/images/ansiedad.jpg" alt="img" />
-        </a>
-        <a href="#">
-          <img src="/images/video (22).jpg" alt="img" />
-        </a>
-        <a href="https://www.youtube.com/watch?v=gp3L6Ys6nhk">
-          <img src="/images/creltan.jpg" alt="img" />
-        </a>
-        <a href="#">
-          <img src="/images/video (8).jpg" alt="img" />
-        </a>
-        <a href="https://www.youtube.com/watch?v=0nHBt6PztQQ">
-          <img src="/images/one-plane.jpg" alt="img" />
-        </a>
-        <a href="#">
-          <img src="/images/video (16).jpg" alt="img" />
-        </a>
-        <a href="https://www.youtube.com/watch?v=vHnaEPIaoE8">
-          <img src="/images/rukair.jpg" alt="img" />
-        </a>
+      <div className="grid lg:grid-cols-3 grid-cols-1">
+        <div className="relative">
+          <a
+            href="https://www.youtube.com/watch?v=xm9JnzLyNkE"
+            target="_blank"
+            className="group"
+          >
+            <div className="absolute z-30 w-full h-full flex flex-col justify-center items-center gap-2 opacity-0 group-hover:opacity-100 transition-opacity">
+              <span className="text-white font-bold text-lg">
+                HORAS Y HORAS
+              </span>
+              <span className="text-white font-medium text-sm">FEDELUCKER</span>
+            </div>
+            <img
+              src="/images/horas-y-horas.jpg"
+              alt="img"
+              className="absolute z-10 min-w-full min-h-full"
+            />
+          </a>
+        </div>
+
+        <div className="relative">
+          <a
+            href="https://www.youtube.com/watch?v=HuspyfNwJvE"
+            target="_blank"
+            className="group"
+          >
+            <div className="absolute z-30 w-full h-full flex flex-col justify-center items-center gap-2 opacity-0 group-hover:opacity-100 transition-opacity">
+              <span className="text-white font-bold text-lg">TALENTO Y FE</span>
+              <span className="text-white font-medium text-sm"></span>
+            </div>
+            <img
+              src="/images/talento-y-fe.jpg"
+              alt="img"
+              className="absolute z-10 min-w-full min-h-full"
+            />
+          </a>
+        </div>
+
+        <div className="relative">
+          <a href="https://www.youtube.com/watch?v=a5Lj3Lnm7LI" target="_blank" className="group">
+          <div className="absolute z-30 w-full h-full flex flex-col justify-center items-center gap-2 opacity-0 group-hover:opacity-100 transition-opacity">
+              <span className="text-white font-bold text-lg">PALESTINA</span>
+              <span className="text-white font-medium text-sm"></span>
+            </div>
+            <img src="/images/palestina.jpg" alt="img" />
+          </a>
+        </div>
+
+        <div className="relative">
+          <a href="#" target="_blank" className="group">
+            <div className="absolute z-30 w-full h-full flex flex-col justify-center items-center gap-2 opacity-0 group-hover:opacity-100 transition-opacity">
+              <span className="text-white font-bold text-lg">VIDEO</span>
+              <span className="text-white font-medium text-sm"></span>
+            </div>
+            <img src="/images/video (12).jpg" alt="img" />
+          </a>
+        </div>
+
+        <div className="relative">
+          <a href="https://www.youtube.com/watch?v=VvUOLPBSY94" target="_blank" className="group">
+          <div className="absolute z-30 w-full h-full flex flex-col justify-center items-center gap-2 opacity-0 group-hover:opacity-100 transition-opacity">
+              <span className="text-white font-bold text-lg">OPIUM</span>
+              <span className="text-white font-medium text-sm"></span>
+            </div>
+            <img src="/images/opium.jpg" alt="img" />
+          </a>
+        </div>
+
+        <div className="relative">
+          <a href="https://www.youtube.com/watch?v=_DoXKmH_n5w" target="_blank" className="group">
+          <div className="absolute z-30 w-full h-full flex flex-col justify-center items-center gap-2 opacity-0 group-hover:opacity-100 transition-opacity">
+              <span className="text-white font-bold text-lg">ANGELINA JOLIE</span>
+              <span className="text-white font-medium text-sm"></span>
+            </div>
+            <img src="/images/angelina.jpg" alt="img" />
+          </a>
+        </div>
+
+        <div className="relative">
+          <a href="https://www.youtube.com/watch?v=PPbQst-km9c" target="_blank" className="group">
+          <div className="absolute z-30 w-full h-full flex flex-col justify-center items-center gap-2 opacity-0 group-hover:opacity-100 transition-opacity">
+              <span className="text-white font-bold text-lg">ANSIEDAD</span>
+              <span className="text-white font-medium text-sm"></span>
+            </div>
+            <img src="/images/ansiedad.jpg" alt="img" />
+          </a>
+        </div>
+
+        <div className="relative">
+          <a href="#" target="_blank" className="group">
+            <div className="absolute z-30 w-full h-full flex flex-col justify-center items-center gap-2 opacity-0 group-hover:opacity-100 transition-opacity">
+              <span className="text-white font-bold text-lg">VIDEO</span>
+              <span className="text-white font-medium text-sm"></span>
+            </div>
+            <img src="/images/video (22).jpg" alt="img" />
+          </a>
+        </div>
+
+        <div className="relative">
+          <a href="https://www.youtube.com/watch?v=gp3L6Ys6nhk" target="_blank" className="group">
+          <div className="absolute z-30 w-full h-full flex flex-col justify-center items-center gap-2 opacity-0 group-hover:opacity-100 transition-opacity">
+              <span className="text-white font-bold text-lg">CRELTAN</span>
+              <span className="text-white font-medium text-sm"></span>
+            </div>
+            <img src="/images/creltan.jpg" alt="img" />
+          </a>
+        </div>
+
+        <div className="relative">
+          <a href="#" target="_blank" className="group">
+            <div className="absolute z-30 w-full h-full flex flex-col justify-center items-center gap-2 opacity-0 group-hover:opacity-100 transition-opacity">
+              <span className="text-white font-bold text-lg">VIDEO</span>
+              <span className="text-white font-medium text-sm"></span>
+            </div>
+            <img src="/images/video (8).jpg" alt="img" />
+          </a>
+        </div>
+
+        <div className="relative">
+          <a href="https://www.youtube.com/watch?v=0nHBt6PztQQ" target="_blank" className="group">
+          <div className="absolute z-30 w-full h-full flex flex-col justify-center items-center gap-2 opacity-0 group-hover:opacity-100 transition-opacity">
+              <span className="text-white font-bold text-lg">BENDECIDO</span>
+              <span className="text-white font-medium text-sm">ONE PLANE</span>
+            </div>
+            <img src="/images/one-plane.jpg" alt="img" />
+          </a>
+        </div>
+
+        <div className="relative">
+          <a href="#" target="_blank" className="group">
+            <div className="absolute z-30 w-full h-full flex flex-col justify-center items-center gap-2 opacity-0 group-hover:opacity-100 transition-opacity">
+              <span className="text-white font-bold text-lg">VIDEO</span>
+              <span className="text-white font-medium text-sm"></span>
+            </div>
+            <img src="/images/video (16).jpg" alt="img" />
+          </a>
+        </div>
+
+        <div className="relative">
+          <a href="https://www.youtube.com/watch?v=vHnaEPIaoE8" target="_blank" className="group">
+          <div className="absolute z-30 w-full h-full flex flex-col justify-center items-center gap-2 opacity-0 group-hover:opacity-100 transition-opacity">
+              <span className="text-white font-bold text-lg">RUKAIR</span>
+              <span className="text-white font-medium text-sm"></span>
+            </div>
+            <img src="/images/rukair.jpg" alt="img" />
+          </a>
+        </div>
       </div>
     </MainLayout>
   );
