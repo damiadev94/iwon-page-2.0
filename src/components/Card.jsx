42199fc1b48b6b806163dacce0cf7522e8e7c3d7
Damian <damian_l@hotmail.es>
2024-09-28 21:03:55 -0300

    styled-prices-page

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,15 +2,18 @@ import React from "react";
 
 function Card({ title, features, oldPrice, actualPrice }) {
   return (
-    <div className="bg-gray-200 py-4 px-2">
-      <h3 className="font-bold 2xl text-center">{title}</h3>
+    <div className="bg-gray-200 py-4 px-2 flex flex-col justify-between">
+      <h3 className="font-bold text-4xl text-center">{title}</h3>
       <p>
-        {features.forEach((element) => {
-          return element + "\n";
+        {features.map((element) => {
+          return <p className="text-center text-lg mb-2">{element}</p>;
         })}
       </p>
-      <span className="line-through block">{oldPrice}</span>
-      <span className="block">{actualPrice}</span>
+      <div>
+
+      <span className="line-through block text-center text-lg">{oldPrice}</span>
+      <span className="block text-center font-bold text-4xl">{actualPrice}</span>
+      </div>
     </div>
   );
 }
--- src/pages/Precios.jsx
+++ src/pages/Precios.jsx
@@ -1,23 +1,77 @@
 import React from "react";
 import MainLayout from "../layout/MainLayout";
 import Card from "../components/Card";
+import { FaWhatsapp } from "react-icons/fa";
 
 function Precios() {
   return (
     <MainLayout>
-      <h1 className="text-white font-bold text-xl text-center my-4 uppercase">
-        Precios
-      </h1>
-      <div className="w-full grid lg:grid-cols-4 grid-cols-1 gap-1 lg:min-h-[728px]">
-        {/* VIDEOCLIP ESTANDAR */}
-        <Card title={"VIDEOCLIP ESTANDAR"} features={["Rodaje de 2 horas","1 Locación", "Edición", "Color", "Filmación con cámara 6k"]} oldPrice={"$119.999"} actualPrice={"$89.999"}/>
-        {/* VIDEOCLIP COMPLETO */}
-        <Card title={"VIDEOCLIP COMPLETO"} features={["Rodaje de 2 horas","Luces led rgb profesionales incluidas", "Ya diponible (vertical) incluido", "Trailer incluido", "Edición", "Color", "Filmación con cámara 6k"]} oldPrice={"$169.999"} actualPrice={"$119.999"}/>
-        {/* VIDEOCLIP BANDA */}
-        <Card title={"VIDEOCLIP BANDA"} features={["Rodaje de 2 horas", "Edición", "Color", "Filmación con cámara 6k", "Ya disponible (vertical) incluido"]} oldPrice={"$199.999"} actualPrice={"$149.999"}/>
-        {/* VIDEOCLIP GUIONADO */}
-        <Card title={"VIDEOCLIP GUIONADO"} features={["Rodaje de 4 horas","Guión", "Edición", "Color", "Filmación con cámara 6k"]} oldPrice={"$249.999"} actualPrice={"$199.999"}/>
-        
+      <div className="flex flex-col gap-8">
+        <h1 className="text-white font-bold text-4xl text-center my-4 uppercase">
+          nuestros precios
+        </h1>
+        <div className="w-full grid lg:grid-cols-4 grid-cols-1 gap-1 lg:min-h-[728px]">
+          {/* VIDEOCLIP ESTANDAR */}
+          <Card
+            title={"VIDEOCLIP ESTANDAR"}
+            features={[
+              "Rodaje de 2 horas",
+              "1 Locación",
+              "Edición",
+              "Color",
+              "Filmación con cámara 6k",
+            ]}
+            oldPrice={"$119.999"}
+            actualPrice={"$89.999"}
+          />
+          {/* VIDEOCLIP COMPLETO */}
+          <Card
+            title={"VIDEOCLIP COMPLETO"}
+            features={[
+              "Rodaje de 2 horas",
+              "Luces led rgb profesionales incluidas",
+              "Ya diponible (vertical) incluido",
+              "Trailer incluido",
+              "Edición",
+              "Color",
+              "Filmación con cámara 6k",
+            ]}
+            oldPrice={"$169.999"}
+            actualPrice={"$119.999"}
+          />
+          {/* VIDEOCLIP BANDA */}
+          <Card
+            title={"VIDEOCLIP BANDA"}
+            features={[
+              "Rodaje de 2 horas",
+              "Edición",
+              "Color",
+              "Filmación con cámara 6k",
+              "Ya disponible (vertical) incluido",
+            ]}
+            oldPrice={"$199.999"}
+            actualPrice={"$149.999"}
+          />
+          {/* VIDEOCLIP GUIONADO */}
+          <Card
+            title={"VIDEOCLIP GUIONADO"}
+            features={[
+              "Rodaje de 4 horas",
+              "Guión",
+              "Edición",
+              "Color",
+              "Filmación con cámara 6k",
+            ]}
+            oldPrice={"$249.999"}
+            actualPrice={"$199.999"}
+          />
+        </div>
+        <div className="bg-gray-50 rounded-md py-2 px-4 flex gap-2 w-fit mx-auto">
+          <FaWhatsapp />
+          <a href="https://wa.link/4iun7m" target="_blank" className="text-lg font-bold">
+            HABLANOS AL WP
+          </a>
+        </div>
       </div>
     </MainLayout>
   );
